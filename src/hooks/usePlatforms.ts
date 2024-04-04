a61cdcbd677e1610c7b7ebbb7c2a3348a753fb7a
San Shwe <sanxhwe@gmail.com>
2024-04-04 21:00:38 +0630

    Hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,10 +1,6 @@
 import platforms from "../data/platforms";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+
 
 const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
 
